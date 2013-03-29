0e36376d19086ef278c4c1abdec7826db4ec69d7
giannif <giannif@gmail.com>
2013-03-28 21:25:46 -0400

    adding FullScreen util

--- dist/mtvn-util.js
+++ dist/mtvn-util.js
@@ -3576,6 +3576,58 @@
         return data;
     };
     
+    Util.FullScreen = function(document) {
+    	var cancelFunc = document.cancelFullScreen || document.mozCancelFullScreen || document.webkitCancelFullScreen,
+    		FullScreen = _.extend({}, Backbone.Events, {
+    			toggle: function(element) {
+    				if (FullScreen.isFullScreen()) {
+    					return FullScreen.cancelFullScreen();
+    				} else {
+    					return FullScreen.requestFullScreen(element);
+    				}
+    			},
+    			requestFullScreen: function(element) {
+    					element = element || document.documentElement;
+    				var requestFunc = element.requestFullScreen || element.mozRequestFullScreen || element.webkitRequestFullScreen;
+    				if (requestFunc) {
+    					requestFunc.apply(element);
+    					return true;
+    				}
+    				return false;
+    			},
+    			cancelFullScreen: function() {
+    				if (cancelFunc) {
+    					cancelFunc();
+    					return true;
+    				}
+    				return false;
+    			},
+    			isFullScreen: function() {
+    				return ((document.fullScreenElement !== undefined && document.fullScreenElement !== null) || (document.mozFullScreen !== undefined && document.mozFullScreen === true) || (document.webkitIsFullScreen !== undefined && document.webkitIsFullScreen === true));
+    			},
+    			Events: {
+    				FULL_SCREEN_CHANGE: "fullscreenChange"
+    			}
+    		}),
+    		vendorFullscreenChange;
+    	if (document.fullScreenEnabled || document.fullscreenEnabled) {
+    		vendorFullscreenChange = "fullscreenchange";
+    	} else if (document.mozFullscreenEnabled || document.mozFullScreenEnabled) {
+    		vendorFullscreenChange = "mozfullscreenchange";
+    	} else if (document.webkitFullscreenEnabled || document.webkitFullScreenEnabled) {
+    		vendorFullscreenChange = "webkitfullscreenchange";
+    	}
+    	if (vendorFullscreenChange) {
+    		document.addEventListener(vendorFullscreenChange, function() {
+    			FullScreen.trigger(FullScreen.Events.FULL_SCREEN_CHANGE, {
+    				type: FullScreen.Events.FULL_SCREEN_CHANGE,
+    				data: FullScreen.isFullScreen()
+    			});
+    		}, false);
+    	}
+    	return FullScreen;
+    }(document);	
+    
     // copy Backbone's extend method.
     Util.extend = Backbone.Model.extend;
     
@@ -3586,4 +3638,4 @@
     Backbone.noConflict();
 })(this);
 MTVNPlayer.require("mtvn-util").version = "0.2.0";
-MTVNPlayer.require("mtvn-util").build = "2013-03-21 12:03:35";
+MTVNPlayer.require("mtvn-util").build = "2013-03-28 09:03:36";
